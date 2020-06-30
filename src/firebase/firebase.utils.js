5631276001e839d97398f8ac0427c710e41cd73b
zye88 <zhengzheng.ye21@gmail.com>
2020-06-30 13:01:58 -0400

    Changed to static firebase config for testing

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -4,13 +4,20 @@ import 'firebase/auth';
 import 'firebase/firestore';
 
 const config = {
-    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-    databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
-    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-    appId: process.env.REACT_APP_FIREBASE_APP_ID
+  apiKey: "AIzaSyBWKYHBH1TWhmCKh9vU-X6bloD8yFKg_OI",
+    authDomain: "planteen-c72b5.firebaseapp.com",
+    databaseURL: "https://planteen-c72b5.firebaseio.com",
+    projectId: "planteen-c72b5",
+    storageBucket: "planteen-c72b5.appspot.com",
+    messagingSenderId: "1085248095635",
+    appId: "1:1085248095635:web:f0c5a82288456d0a301466"
+    // apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    // authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+    // databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+    // projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+    // storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+    // messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+    // appId: process.env.REACT_APP_FIREBASE_APP_ID
   };
 
 firebase.initializeApp(config);
